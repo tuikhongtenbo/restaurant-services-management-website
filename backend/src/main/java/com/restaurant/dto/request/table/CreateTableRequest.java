f56cf17348be0a36ade94f889e1854b636648c68
thybui1903 <thybui1903qn@gmail.com>
2026-05-29 12:01:02 +0700

    modified:   backend/src/main/java/com/restaurant/controller/table/TableController.java #	modified:   backend/src/main/java/com/restaurant/dto/request/table/CreateTableRequest.java #	modified:   backend/src/main/java/com/restaurant/dto/request/table/OpenTableRequest.java #	modified:   backend/src/main/java/com/restaurant/dto/request/table/UpdateTableRequest.java #	modified:   backend/src/main/java/com/restaurant/dto/response/table/TableLayoutResponse.java #	modified:   backend/src/main/java/com/restaurant/dto/response/table/TableResponse.java #	modified:   backend/src/main/java/com/restaurant/model/Table.java #	modified:   backend/src/main/java/com/restaurant/repository/TableRepository.java #	modified:   backend/src/main/java/com/restaurant/service/table/TableService.java #	new file:   backend/src/main/java/com/restaurant/service/table/impl/TableServiceimpl.java #

--- backend/src/main/java/com/restaurant/dto/request/table/CreateTableRequest.java
+++ backend/src/main/java/com/restaurant/dto/request/table/CreateTableRequest.java
@@ -1,10 +1,22 @@
 package com.restaurant.dto.request.table;
 
-// THY
-// TODO:
-// @NotBlank String number
-// @NotNull @Min(1) Integer capacity
-// String area (Tang_1|Tang_2|San_vuon|Phong_VIP)
-// Integer posX, Integer posY (keo tha)
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
 public class CreateTableRequest {
+
+    @NotBlank(message = "So ban khong duoc trong")
+    @Size(max = 10, message = "So ban khong qua 10 ky tu")
+    private String number;
+
+    @NotNull(message = "Suc chua khong duoc trong")
+    @Min(value = 1, message = "Suc chua phai lon hon 0")
+    private Integer capacity;
+
+    @Size(max = 50, message = "Khu vuc khong qua 50 ky tu")
+    private String area;
 }
